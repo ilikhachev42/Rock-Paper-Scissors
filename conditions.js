66f2695f2e0ee3cd8c5cd7169bb5212b40d55d9c
justkoi42 <justkoi42@gmail.com>
2023-06-27 15:38:37 +0530

    Write an array with objects. Each object is win, lose or tie condition for the game results.

---
+++ conditions.js
@@ -0,0 +1,38 @@
+const conditions = [
+	{
+		user: 'rock',
+		computer: 'scissors'
+	},
+	{
+		user: 'scissors',
+		computer: 'paper'
+	},
+	{
+		user: 'paper',
+		computer: 'rock'
+	},
+	{
+		user: 'scissors',
+		computer: 'rock'
+	},
+	{
+		user: 'paper',
+		computer: 'scissors'
+	},
+	{
+		user: 'rock',
+		computer: 'paper'
+	},
+	{
+		user: 'rock',
+		computer: 'rock'
+	},
+	{
+		user: 'paper',
+		computer: 'paper'
+	},
+	{
+		user: 'scissors',
+		computer: 'scissors'
+	}
+]
